493464b10a7d5f184bc40bc60dee755ea2f80bba
Peter Racine Jr <peterracinejr@gmail.com>
2021-09-07 15:31:15 -0500

    initial setup

---
+++ GarysWholesaleGarage/Tesla.cs
@@ -0,0 +1,14 @@
+namespace GarysWholesaleGarage
+{
+    public class Tesla  // Electric car
+    {
+        public double BatteryKWh { get; set; }
+        public string MainColor { get; set; }
+        public int MaximumOccupancy { get; set; }
+
+        public void ChargeBattery()
+        {
+            // method definition omitted
+        }
+    }
+}
